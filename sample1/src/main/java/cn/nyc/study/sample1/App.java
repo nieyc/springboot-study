b3198879f869206172440f92f7afc13a0cc8c889
nieyc <nieyc@cssweb.sh.cn>
2017-12-21 10:39:01 +0800

    first commit

---
+++ sample1/src/main/java/cn/nyc/study/sample1/App.java
@@ -0,0 +1,13 @@
+package cn.nyc.study.sample1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
